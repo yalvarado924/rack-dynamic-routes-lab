c1a0dc4d134d55328ce286e41ee80a15d5659bbe
Yuliana Alvarado <yalvarado924@gmail.com>
2021-01-27 16:38:49 -0600

    commit tree 65e247addb0f3219d926e012cb56a2aa6a18d980

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+    @@items = [Item.new("Figs", 3.24), Item.new("Pears", 0.99)]
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match (/items/)
+            item_name =  req.path.split("/items/").last
+            if item = @@items.find do |i|
+                i.name == item_name
+            end
+                resp.write "#{item.price}"
+            else
+                resp.write "Item not found"
+                resp.status = 400    
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+end
